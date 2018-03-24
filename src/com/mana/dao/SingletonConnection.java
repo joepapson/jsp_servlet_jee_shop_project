687ef29b131f4f053f6e819b2e6d219d3243de9f
Pierre Lambo <pierrelambo@googlemail.com>
2018-03-24 15:16:58 +0100

    password changed

--- src/com/mana/dao/SingletonConnection.java
+++ src/com/mana/dao/SingletonConnection.java
@@ -12,7 +12,7 @@ public class SingletonConnection {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/product_shop", "admin", "Stepstone_12");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/product_shop", "admin", "yourPassword");
 		} catch (ClassNotFoundException e) {
 			
 			e.printStackTrace();
